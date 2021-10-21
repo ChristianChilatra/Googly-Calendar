6ad122652157caf4438a20a105aaefbf37d4d068
Christian Chilatra <chilatra1998@gmail.com>
2021-10-21 01:54:22 -0500

    test

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-import { intervalCurrentTimePosition } from './current-time.js';
-import './task.js';
-intervalCurrentTimePosition(5000)
+const configState = {
+  open : true
+}
+
+const $buttonSideBar = document.querySelector(".iconSidebar")
+const $elementSideBar = document.querySelector(".sideBar")
+const $elementCalendar = document.querySelector(".calendar")
+const $inlineSizeSideBar = $elementSideBar.getBoundingClientRect().width
+
+console.log($inlineSizeSideBar)
+$buttonSideBar.addEventListener("click", showSideBar)
+
+
+
+function showSideBar(event) {
+  $elementSideBar.style.transition = "inset-inline-start .3s"
+  if(configState.open){
+    $elementSideBar.style.insetInlineStart = `-${$inlineSizeSideBar}px`
+    $elementCalendar.style.insetInlineStart = `${$inlineSizeSideBar}px`
+    configState.open = false
+  }else{
+    $elementSideBar.style.insetInlineStart = `0px`
+    $elementCalendar.style.insetInlineStart = `${0}px`
+    configState.open = true
+  }
+}
---
+++ js/side-bar.js
@@ -0,0 +1,9 @@
+
+
+const $buttonSideBar = document.querySelector(".iconSidebar")
+
+$buttonSideBar.addEventListener("click",showSideBar)
+
+function showSideBar(event){
+  console.log("ahi vamos")
+}
