daf3ec0af11169e2cc31f953b165e321b02b96c8
Christian Chilatra <chilatra1998@gmail.com>
2021-10-23 04:02:04 -0500

    little modification

--- js/services/data-time.js
+++ js/services/data-time.js
@@ -9,28 +9,32 @@ function setFormatTime(configTime, configDate = date) {
 }
 
 
-export function getDateMonth(){
+export function getDateMonth() {
   return date.getMonth()
 }
-export function getDateMonthAmount(value){
-  const newDate = new Date(date.getFullYear(),value + 1,0).getDate()
+export function getDateMonthAmount(value) {
+  const newDate = new Date(date.getFullYear(), value + 1, 0).getDate()
   return newDate
 }
-export function getDateMonthFormat(configTime, monthCurrent){
+export function getDateMonthFormat(configTime, monthCurrent) {
   const newDate = new Date()
   newDate.setMonth(monthCurrent)
-  let formatDate = setFormatTime(configTime, newDate).split(" ")[2]
-  formatDate = formatDate.replace(formatDate[0],formatDate[0].toUpperCase())
+  let formatDate = setFormatTime(configTime, newDate).split(" ")
+  formatDate.shift()
+  formatDate.shift()
+  formatDate = formatDate.join(" ")
+  formatDate = formatDate.replace(formatDate[0], formatDate[0].toUpperCase())
+
   return formatDate
 }
-export function getDateDay(){
+export function getDateDay() {
   return date.getDay()
 }
-export function getDateTimeFormat(configTime){
+export function getDateTimeFormat(configTime) {
   const formatDate = setFormatTime(configTime).split("de")
   return formatDate.join("")
 }
-export function getDateTimeNumberFormat(configTime){
+export function getDateTimeNumberFormat(configTime) {
   const formatDate = setFormatTime(configTime).split("/").reverse()
   return formatDate.join("-")
 }
